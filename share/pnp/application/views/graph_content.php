5d91f7db1fc7cfdba2c62f0bfdb4ba0a441fe9d3
Charles Lane <lane@duphy4.physics.drexel.edu>
2022-09-19 12:55:44 -0400

    fix to 'basket' display

--- share/pnp/application/views/graph_content.php
+++ share/pnp/application/views/graph_content.php
@@ -87,16 +87,18 @@ foreach($this->data->STRUCT as $key=>$value){
 	# urlencode the graph id, to prevent # chars in service names being 
 	# treated like a url fragment when zooming
 	$gid = array();
-	parse_str(ltrim($this->url, '?'), $gid);
-	$gid = htmlentities("?host=".urlencode($gid["host"])."&srv=".urlencode($gid["srv"]));
+	#parse_str(ltrim($this->url, '?'), $gid);
+	#$gid = htmlentities("?host=".urlencode($gid["host"])."&srv=".urlencode($gid["srv"]));
 	
-	echo "<div start=".$value['TIMERANGE']['start']." end=".$value['TIMERANGE']['end']." style=\"width:".$value['GRAPH_WIDTH']."px; height:".$value['GRAPH_HEIGHT']."px; position:absolute; top:33px\" class=\"graph\" id=\"".$gid."\" ></div>";
 	
 	// build the URI which renders the dynamic graph image
 	$path = array('host'   => $value['MACRO']['HOSTNAME'],
 	              'srv'    => $value['MACRO']['SERVICEDESC'],
 	              'source' => $value['SOURCE']);
-	
+	$gid = htmlentities("?host=".urlencode($path["host"])."&srv=".urlencode($path["srv"]));
+
+	echo "<div start=".$value['TIMERANGE']['start']." end=".$value['TIMERANGE']['end']." style=\"width:".$value['GRAPH_WIDTH']."px; height:".$value['GRAPH_HEIGHT']."px; position:absolute; top:33px\" class=\"graph\" id=\"".$gid."\" ></div>";
+
 	// only include `view` in the querystring if we are in a preset view; likewise, only 
 	// include timerange start/end if we are not in a preset view; this will help later
 	// to differentiate between preset and custom timeranges, for display purposes
