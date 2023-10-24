0805d05b7b13226d55fc4598460fed6a5479b805
Charles Lane <lane@dchooz.org>
2023-10-23 23:12:59 -0400

    forgot to fix this ONE file...

--- share/pnp/application/models/system.php
+++ share/pnp/application/models/system.php
@@ -1,5 +1,6 @@
 <?php
 
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
 // phpcs:disable PSR1.Files.SideEffects
 defined('SYSPATH') or die('No direct access allowed.');
 // phpcs:enable PSR1.Files.SideEffects
