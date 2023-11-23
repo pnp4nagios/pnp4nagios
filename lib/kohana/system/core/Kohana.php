2a9ca3caf65a8facc48fb016880227e38552f385
Charles Lane <lane@dchooz.org>
2023-11-23 10:55:18 -0500

    deal with trivial php lint complaint about Kohana_Exception

--- lib/kohana/system/core/Kohana.php
+++ lib/kohana/system/core/Kohana.php
@@ -1,10 +1,10 @@
 <?php
 
+// phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
 // phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
 // phpcs:disable PSR1.Files.SideEffects
 defined('SYSPATH') or die('No direct access allowed.');
 // phpcs:enable PSR1.Files.SideEffects
-
 
 // phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
 
