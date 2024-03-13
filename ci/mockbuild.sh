f9f1a6bc5cf1951eb1a2f0ba5ddbcb867502223f
Charles Lane <lane@dchooz.org>
2024-03-12 21:17:10 -0400

    more gitbuild fixes?

--- ci/mockbuild.sh
+++ ci/mockbuild.sh
@@ -6,9 +6,9 @@ VERSION=$1
 dnf install -y epel-release
 dnf install -y mock
 
-tar -xf ${NAME}-${VERSION}.tgz ${NAME}-${VERSION}/ci/${NAME}.spec
-mv ${NAME}-${VERSION}/ci/${NAME}.spec ./${NAME}.spec.base
-
+#tar -xf ${NAME}-${VERSION}.tgz ${NAME}-${VERSION}/ci/${NAME}.spec
+#mv ${NAME}-${VERSION}/ci/${NAME}.spec ./${NAME}.spec.base
+cp ${NAME}.spec ${NAME}.spec.base
 
 RELEASE=$(grep '^Release: ' ${NAME}.spec.base | cut -d ':' -f2 | awk -F'%' '{print $1}' | tr -d ' ')
 
