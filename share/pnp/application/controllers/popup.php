e410778cac9a3cf7eb2c8cd84e8e2993d6a73fa3
Chuck Lane <lane@dchooz.org>
2024-03-19 21:30:07 -0400

    Update popup.php
    try to get past PHP error checks

--- share/pnp/application/controllers/popup.php
+++ share/pnp/application/controllers/popup.php
@@ -14,7 +14,6 @@ defined('SYSPATH') or die('No direct access allowed.');
  */
 class Popup_Controller extends System_Controller
 {
-
     public $imgwidth = 0;
     
     public function __construct()
